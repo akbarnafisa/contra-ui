cc8211e7c84319faa142eecac32f130d23c4ce72
Akbar Nafisa Ja’far <akbarnj788@gmail.com>
2021-03-12 23:14:20 +0700

    feat(provider): init provider

---
+++ src/core/provider/components/Provider.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { DefaultTheme, Theme } from '~/core/theme'
+
+interface ProviderProps {
+  theme?: Partial<Theme>
+  children: React.ReactChildren | React.ReactChild
+}
+
+const FONT_URL =
+  'https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap'
+
+const Provider: React.FC<ProviderProps> = ({ children }) => {
+  React.useEffect(() => {
+    const fontLink = document.createElement('link')
+    fontLink.href = FONT_URL
+    fontLink.rel = 'stylesheet'
+    document.head.appendChild(fontLink)
+  }, [])
+
+  return <ThemeProvider theme={DefaultTheme}>{children}</ThemeProvider>
+}
+
+export default Provider

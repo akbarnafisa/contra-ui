a3df2e62416cc9bdb45ec2fe11de4aec9ed6e2af
Akbar Nafisa Ja’far <akbarnj788@gmail.com>
2021-03-13 08:44:45 +0700

    feat(Typography): add body component

---
+++ src/components/Typography/Body/components/Body.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Body from './Body'
+
+export default {
+  title: 'Core/Typography',
+  component: Body
+}
+
+export const BodyTypography = () => (
+  <>
+    <Body level="21">Almost before we knew it, we had left the ground</Body>
+    <Body level="17">Almost before we knew it, we had left the ground</Body>
+    <Body level="15">Almost before we knew it, we had left the ground</Body>
+    <Body level="12">Almost before we knew it, we had left the ground</Body>
+  </>
+)
---
+++ src/components/Typography/Body/components/Body.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components'
+import { variant, color, space, typography, layout } from 'styled-system'
+import {
+  ColorProps,
+  SpaceProps,
+  TypographyProps,
+  LayoutProps
+} from 'styled-system'
+
+const baseVariants = {
+  fontFamily: 'body',
+  color: 'grey09',
+  fontWeight: 'medium',
+  lineHeight: 'body'
+}
+
+const variants = {
+  '21': {
+    ...baseVariants,
+    fontSize: '21'
+  },
+  '17': {
+    ...baseVariants,
+    fontSize: '17'
+  },
+  '15': {
+    ...baseVariants,
+    fontSize: '15'
+  },
+  '12': {
+    ...baseVariants,
+    fontSize: '12'
+  }
+}
+
+export type StyledSystemProps = SpaceProps &
+  ColorProps &
+  LayoutProps &
+  TypographyProps
+
+export interface BodyBaseProps extends StyledSystemProps {
+  className?: string
+  style?: React.CSSProperties
+  level?: '21' | '17' | '15' | '12'
+}
+
+const Body = styled('p')<BodyBaseProps>(
+  {
+    margin: 0
+  },
+  variant({
+    variants,
+    prop: 'level'
+  }),
+  color,
+  space,
+  typography,
+  layout
+)
+
+export default Body

b233d609066e53b764c23570b158a4329bcee99d
Akbar Nafisa Ja’far <akbarnj788@gmail.com>
2021-03-13 11:37:20 +0700

    feat(Flex): add flex component

---
+++ src/components/Layout/Flex/components/Flex.stories.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+
+import Flex from './Flex'
+import { Box } from '~/components/Box'
+
+export default {
+  title: 'Core/Layout',
+  component: Flex
+}
+
+export const FlexLayout = () => (
+  <Box maxWidth="375px" m="0 auto">
+    <Flex variant="center" mb="xxs">
+      <Box bg="primary" p="md" mx="xxxs" />
+      <Box bg="primary" p="md" mx="xxxs" />
+      <Box bg="primary" p="md" mx="xxxs" />
+    </Flex>
+
+    <Flex justifyContent="space-around" mb="xxs">
+      <Box bg="primary" p="md" mx="xxxs" />
+      <Box bg="primary" p="md" mx="xxxs" />
+      <Box bg="primary" p="md" mx="xxxs" />
+    </Flex>
+
+    <Flex justifyContent="flex-end" mb="xxs">
+      <Box bg="primary" p="md" mx="xxxs" />
+      <Box bg="primary" p="md" mx="xxxs" />
+      <Box bg="primary" p="md" mx="xxxs" />
+    </Flex>
+  </Box>
+)
---
+++ src/components/Layout/Flex/components/Flex.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+import { variant } from 'styled-system'
+import { Box } from '~/components/Box'
+
+export interface FlexProps {
+  variant?: 'center'
+}
+const Flex = styled(Box)<FlexProps>(
+  {
+    display: 'flex'
+  },
+  variant({
+    variants: {
+      center: {
+        alignItems: 'center',
+        justifyContent: 'center'
+      }
+    }
+  })
+)
+
+export default Flex

2f649dd5f891b4499c7d22eaed4bcecbdfb70cbd
Akbar Nafisa Ja’far <akbarnj788@gmail.com>
2021-03-27 11:10:43 +0700

    chore(utils): add props utils

---
+++ packages/contra-ui-core/src/utils/props.ts
@@ -0,0 +1,58 @@
+import {
+  layout,
+  LayoutProps,
+  flexbox,
+  FlexboxProps,
+  position,
+  PositionProps,
+  grid,
+  GridProps,
+  space,
+  SpaceProps,
+  background,
+  BackgroundProps,
+  color,
+  ColorProps,
+  border,
+  BorderProps,
+  shadow,
+  ShadowProps,
+  typography,
+  TypographyProps,
+  compose,
+} from 'styled-system'
+
+import {
+  createShouldForwardProp,
+  props,
+} from '@styled-system/should-forward-prop'
+
+export function sfp(p: string[] = []) {
+  return createShouldForwardProp([...props, ...p])
+}
+
+export const shouldForwardProp = createShouldForwardProp(props);
+
+export type AllSystemProps = LayoutProps &
+  FlexboxProps &
+  PositionProps &
+  GridProps &
+  SpaceProps &
+  BackgroundProps &
+  ColorProps &
+  BorderProps &
+  ShadowProps &
+  TypographyProps
+
+export const allSystemProps = compose(
+  layout,
+  flexbox,
+  position,
+  grid,
+  space,
+  background,
+  color,
+  border,
+  shadow,
+  typography
+)

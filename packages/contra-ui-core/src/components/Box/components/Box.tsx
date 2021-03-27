cace508c0a7ce7a64c381a9fd8c24c63e13aae32
Akbar Nafisa Ja’far <akbarnj788@gmail.com>
2021-03-27 11:08:55 +0700

    feat(Box): update to use all system props

--- packages/contra-ui-core/src/components/Box/components/Box.tsx
+++ packages/contra-ui-core/src/components/Box/components/Box.tsx
@@ -1,29 +1,13 @@
 import styled from 'styled-components'
-import {
-  space,
-  color,
-  border,
-  layout,
-  flexbox,
-  shadow,
-  position,
-  grid,
-} from 'styled-system'
-import shouldForwardProp from '@styled-system/should-forward-prop'
+import { allSystemProps, shouldForwardProp } from '~/utils/props'
+
 import { BoxSystemProps } from './BoxTypes'
 
 const Box = styled('div').withConfig<BoxSystemProps>({ shouldForwardProp })(
   {
     boxSizing: 'border-box',
   },
-  space,
-  color,
-  border,
-  layout,
-  flexbox,
-  shadow,
-  position,
-  grid
+  allSystemProps
 )
 
 export default Box

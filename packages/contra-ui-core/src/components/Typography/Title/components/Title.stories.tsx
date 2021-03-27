9a9d2d28cc419b51f6c95c54f7c1979eb7756d50
Akbar Nafisa Ja’far <akbarnj788@gmail.com>
2021-03-27 11:17:14 +0700

    docs(Title): update storybook for Title component

--- packages/contra-ui-core/src/components/Typography/Title/components/Title.stories.tsx
+++ packages/contra-ui-core/src/components/Typography/Title/components/Title.stories.tsx
@@ -8,6 +8,7 @@ export default {
 
 export const TitleTypography = () => (
   <>
+    <Title level="54">Almost before we knew it, we had left the ground</Title>
     <Title level="44">Almost before we knew it, we had left the ground</Title>
     <Title level="36">Almost before we knew it, we had left the ground</Title>
     <Title level="27">Almost before we knew it, we had left the ground</Title>

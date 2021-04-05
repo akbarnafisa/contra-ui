30cf533afc92889fa487054f9c000ab49f7deabc
Akbar Nafisa Ja’far <akbar.nafisa@dana.id>
2021-04-05 22:03:12 +0700

    chore: update example script and next config

--- packages/example/next.config.js
+++ packages/example/next.config.js
@@ -1,4 +1,8 @@
-const withTM = require('next-transpile-modules')([
-  '@contra-ui/core',
-])
-module.exports = withTM()
+const withPlugins = require('next-compose-plugins')
+
+const withTM = require('next-transpile-modules')(['@contra-ui/core'])
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withPlugins([[withBundleAnalyzer], [withTM]])

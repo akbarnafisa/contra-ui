efe8351540fded23cbd5d13483f3a9fb5a291e6b
Akbar Nafisa Ja’far <akbar.nafisa@dana.id>
2021-04-06 15:42:23 +0700

    chore: init rollup build

---
+++ packages/contra-ui-core/rollup.config.js
@@ -0,0 +1,79 @@
+import globby from 'globby'
+import excludeDependenciesFromBundle from 'rollup-plugin-exclude-dependencies-from-bundle'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+import { terser } from 'rollup-plugin-terser'
+
+const packageJson = require('./package.json')
+
+const globals = {
+  ...Object.keys(packageJson.dependencies || {}),
+  ...Object.keys(packageJson.peerDependencies || {}),
+}
+
+function generateComponentInput(pathList) {
+  return pathList.reduce((acc, curr) => {
+    const filename = curr.match(/([^\/]+)(?=\.\w+$)/)[0]
+    return {
+      ...acc,
+      [filename]: curr,
+    }
+  }, {})
+}
+
+export default globby([
+  'src/**/*.tsx',
+  '!src/**/*.stories.tsx',
+  '!src/**/__stories__/**/*.tsx',
+])
+  .then(path => generateComponentInput(path))
+  .then(componentInput => [
+    {
+      input: {
+        index: 'src/index.ts',
+        ...componentInput,
+      },
+      output: [
+        {
+          dir: 'dist/esm',
+          format: 'esm',
+        },
+      ],
+      plugins: [
+        excludeDependenciesFromBundle(),
+        resolve(),
+        commonjs(),
+        typescript({
+          useTsconfigDeclarationDir: true,
+        }),
+        commonjs({
+          exclude: ['./node_modules', '../../node_modules'],
+          ignoreGlobal: true,
+        }),
+        terser()
+      ],
+      external: Object.keys(globals),
+    },
+    {
+      input: 'src/index.ts',
+      output: [
+        {
+          file: packageJson.main,
+          format: 'cjs',
+        },
+      ],
+      plugins: [
+        excludeDependenciesFromBundle(),
+        resolve(),
+        commonjs(),
+        typescript({ tsconfig: './tsconfig.json' }),
+        commonjs({
+          exclude: ['./node_modules', '../../node_modules'],
+          ignoreGlobal: true,
+        }),
+        terser()
+      ],
+      external: Object.keys(globals),
+    },
+  ])
